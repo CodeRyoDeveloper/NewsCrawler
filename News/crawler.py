c1ffd6f1cd2ad5c622f299a1d6a313098dd038f2
RRAaru <100890852+RRAaru@users.noreply.github.com>
2022-07-07 14:54:35 +0800

    修復日期名稱與抓取資料日期不符的問題

--- News/crawler.py
+++ News/crawler.py
@@ -1,6 +1,5 @@
 import urllib.request as req    
 import bs4  
-import datetime
 def getdata(url):
     request=req.Request(url,headers={
         "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
@@ -10,9 +9,11 @@ def getdata(url):
     soup=bs4.BeautifulSoup(data,"html.parser")
     root=soup.find_all("div",class_="cd__content")
     for root in root:
+        Del=".!@#$%^&*()\/:*?<>|-+ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz "
         characters="\/:*?<>|"
+        date= ''.join( x for x in str(root.a.get("href")) if x not in Del)
         tit = ''.join( x for x in root.text if x not in characters)
-        file=open(str(today)+" "+tit+".txt",mode="w",encoding='UTF-8')
+        file=open(date[0:8]+" "+tit+".txt",mode="w",encoding='UTF-8')
         print(root.text)
         file.write(root.text+"\n")
         print("https://edition.cnn.com"+root.a.get("href"))
@@ -33,5 +34,4 @@ def pagedata(url):
     else:
         return "None"
 url="https://edition.cnn.com/business"
-today = (datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=-4))).strftime("%Y-%m-%d"))
 getdata(url)
